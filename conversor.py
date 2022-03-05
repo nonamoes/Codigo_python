50404f675d6762e23e312980c9f5f4678d033340
nonamoes <chiusagui@gmail.com>
2022-03-05 11:47:36 -0600

    Update conversor.py
    con # se comentan lineas

--- conversor.py
+++ conversor.py
@@ -2,6 +2,6 @@ pesos = input("¿Cuántos pesos mxn tienes?: ")
 pesos = float(pesos)
 valor_dolar = 20.95
 dolares= pesos / valor_dolar 
-dolares = round(dolares, 2) 
+dolares = round(dolares, 2) #redondeo
 dolares = str(dolares)
 print("Tienes $" + dolares+ " dólares.")

23f79fc9dbfe39f512991f1f1f4ab363b9637b44
nonamoes <chiusagui@gmail.com>
2022-03-07 07:48:37 -0600

    Create for.py
    usando for en py

---
+++ for.py
@@ -0,0 +1,19 @@
+#Imprimir un numero del 1 al 1000 
+
+#ciclo while 
+#contador = 1
+#print (contador)
+#while contador < 1000:
+#    contador += 1
+#    print(contador)
+
+
+#ciclo for
+
+for contador in range (1, 1001): #1001 porque el rango llega a un num antes
+    print(contador)
+
+
+#Tabla del 11
+for i in range(10):
+    print(11 * i)

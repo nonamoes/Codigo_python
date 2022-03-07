ecfa58c05cc5be0ecfc09282dcacfdbe08145510
nonamoes <chiusagui@gmail.com>
2022-03-07 08:31:30 -0600

    Create numprimo.py
    Funcion para saber que numero es primo con py

---
+++ numprimo.py
@@ -0,0 +1,25 @@
+def es_primo(numero):
+    contador= 0
+
+    for i in range(1, numero+1):
+        if i == 1 or i == numero: #nos saltamos la linea si esto si una de estas dos condiciones se cumple
+            continue
+        if numero % i == 0:
+            contador += 1
+    if contador == 0:
+        return True
+    else:
+        return False
+
+
+
+
+def run():
+    numero = int(input("Escribe un numero: "))
+    if es_primo(numero): #llama la funcion es_primo
+        print("Es primo")
+    else:
+        print("No es primo")
+    
+if __name__ == "__main__":
+    run()

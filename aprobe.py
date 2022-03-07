9b70bb55b4c3e9ec812217d18ab284db43e36c48
nonamoes <chiusagui@gmail.com>
2022-03-07 14:06:12 -0600

    Update aprobe.py
    twitter de fac :3

--- aprobe.py
+++ aprobe.py
@@ -4,5 +4,5 @@
 
 es_aprobe = "¡Aprobé el Curso Básico de Python en @Platzi!\n"
 diploma_platzi = "https://platzi.com/p/nonamoes/curso/1937-course/diploma/detalle/"
-
-print ("hola mundo :3 " +es_aprobe + diploma_platzi )
+gracias ="\n Gracias a @facmartoni"
+print ("hola mundo :3 " +es_aprobe + diploma_platzi + gracias )

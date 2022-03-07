4cbe2dc161be6ebd57aeaef3b7c7edaf278d5be2
nonamoes <chiusagui@gmail.com>
2022-03-07 08:00:16 -0600

    Create recorrer.py
    como recorrer string con ciclo for

---
+++ recorrer.py
@@ -0,0 +1,13 @@
+def run():
+    # nombre = input("Escribe tu nombre: ")
+    # for letra in nombre:  #para als letras en el nombre vamos a imprimir cada letra en cada vuelta del ciclo
+    #     print(letra)
+    
+    frase = input("Escribe una frase: ")
+    for caracter in frase:
+        print(caracter.upper()) #pone la frase en mayuscula
+        
+
+
+if __name__ == "__main__":
+    run()

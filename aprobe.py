75505edae500e9896cc1c04d5cca28baf06fdc4f
nonamoes <chiusagui@gmail.com>
2022-03-07 13:58:29 -0600

    Create aprobe.py
    Aprobe el curso basico de Python en platzi

---
+++ aprobe.py
@@ -0,0 +1,8 @@
+# Al fin se me hizo conocer 🐍 ^^
+# hello_world.py
+
+
+es_aprobe = "¡Aprobé el Curso Básico de Python en @Platzi!\n"
+diploma_platzi = "https://platzi.com/p/nonamoes/curso/1937-course/diploma/detalle/"
+
+print ("hola mundo :3 " +es_aprobe + diploma_platzi )
